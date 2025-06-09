2f5db5f532476f51608a0f7d6f8385d908233848
Spanos <nickspanosf1@gmail.com>
2025-06-09 16:24:55 +0300

    Add the LanguageChanger dynamic

--- components/LanguageChanger/LanguageChanger.js
+++ components/LanguageChanger/LanguageChanger.js
@@ -17,30 +17,22 @@ function LanguageChanger() {
         <div id="language-container">
             <div className="d-flex justify-content-between languageBtn " data-bs-toggle="collapse"
                  data-bs-target="#languageCollapse" aria-expanded="false" aria-controls="languageCollapse">
-                <div><span className="flag-icon flag-icon-grc"></span>
+                <div><span className={`flag-icon flag-icon-${currentLocale}`}></span>
                 </div>
                 <div></div>
             </div>
             <div className="collapse" id="languageCollapse">
-                <span className="d-none" onClick={() => changeLanguage('greek', '')}> <span
-                    className="flag-icon flag-icon-grc mx-1"></span>
-            </span>
-            <span className="d-none" onClick={() => changeLanguage('english', '')}> <span
-                className="flag-icon flag-icon-usa mx-1"></span> </span>
 
                 {locales.map((locale) => {
-                    const isActive = locale === currentLocale;
-
                     return (
                         <Link
                             key={locale}
                             href={pathname}
                             locale={locale}
-                            className={`px-3 py-1 rounded ${
-                                isActive ? 'bg-black text-white' : 'bg-gray-200 text-black'
-                            }`}
                         >
-                            {locale.toUpperCase()}
+                              <span className="" > <span
+                                  className={`flag-icon flag-icon-${locale} mx-1`}></span>
+            </span>
                         </Link>
                     );
                 })}
