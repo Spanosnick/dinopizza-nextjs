080cc41592ecf88b338b6d599edf98e9587633fb
Spanos <nickspanosf1@gmail.com>
2025-06-19 00:18:25 +0300

    Add dynamic category page /menu/[category]

--- components/LanguageChanger/LanguageChanger.js
+++ components/LanguageChanger/LanguageChanger.js
@@ -2,40 +2,61 @@
 import React from 'react';
 import {useRouter, usePathname,Link} from '@/src/i18n/navigation';
 import {useLocale} from 'next-intl';
+import {getLocalizedCategorySlug, getLocalizedProductSlug} from "@/lib/dataHelpers";
 
 const locales = ['en', 'el'];
 
 
 function LanguageChanger() {
-    const router = useRouter();
     const pathname = usePathname();
     const currentLocale = useLocale();
+    const parts = pathname.split('/').filter(Boolean); // ['en', 'menu', 'pizzas', 'ala-creme']
+
+    const [ section, categorySlug, productSlug] = parts;
+    console.log(parts)
     return (
         <div id="language-container">
-            <div className="d-flex justify-content-between languageBtn " data-bs-toggle="collapse"
-                 data-bs-target="#languageCollapse" aria-expanded="false" aria-controls="languageCollapse">
-                <div><span className={`flag-icon flag-icon-${currentLocale}`}></span>
+            <div
+                className="d-flex justify-content-between languageBtn"
+                data-bs-toggle="collapse"
+                data-bs-target="#languageCollapse"
+                aria-expanded="false"
+                aria-controls="languageCollapse"
+            >
+                <div>
+                    <span className={`flag-icon flag-icon-${currentLocale}`}></span>
                 </div>
                 <div></div>
             </div>
             <div className="collapse" id="languageCollapse">
-
                 {locales.map((locale) => {
+                    if (locale === currentLocale) return null;
+
+                    let href = `/`;
+
+                    if (section === 'menu' && categorySlug) {
+                        const localizedCategory = getLocalizedCategorySlug(categorySlug, currentLocale, locale);
+                        console.log(`Localized category for ${categorySlug} from ${currentLocale} to ${locale}: ${localizedCategory}`);
+                        href += `/menu/${localizedCategory}`;
+
+
+                        if (productSlug) {
+                            const localizedProduct = getLocalizedProductSlug(categorySlug, productSlug, currentLocale, locale);
+                            href += `/${localizedProduct}`;
+                        }
+                    }
+
                     return (
-                        <Link
-                            key={locale}
-                            href={pathname}
-                            locale={locale}
-                        >
-                              <span className="" > <span
-                                  className={`flag-icon flag-icon-${locale} mx-1`}></span>
-            </span>
+                        <Link key={locale} href={href} locale={locale}>
+              <span className="">
+                <span className={`flag-icon flag-icon-${locale} mx-1`}></span>
+              </span>
                         </Link>
                     );
                 })}
+            </div>
         </div>
-</div>
-);
+    );
 }
 
 export default LanguageChanger;
--- src/app/[locale]/menu/[category]/page.js
+++ src/app/[locale]/menu/[category]/page.js
@@ -1,7 +1,29 @@
-export default function Category() {
-    return (
-        <>
-            <h1>This is Category</h1>
-        </>
-    )
+import data from '@/data/products.json';
+import {findCategoryBySlug} from "@/lib/dataHelpers";
+import Category from "@/components/Products/Category";
+
+export async function generateStaticParams() {
+    const paths = [];
+
+    data.categories.forEach((category) => {
+        Object.keys(category.slug).forEach((locale) => {
+            paths.push({
+                locale,
+                category: category.slug[locale],
+            });
+        });
+    });
+
+    return paths;
+}
+
+export default function CategoryPage({params}) {
+    const {locale, category} = params;
+    const foundCategory = findCategoryBySlug(category, locale);
+    if (!foundCategory) {
+        return <div>Κατηγορία δεν βρέθηκε</div>;
+    }
+
+    return <Category category={foundCategory}/>
+
 }
