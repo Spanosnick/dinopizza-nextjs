2fce60a725da74458e5176e934d580a112ee8b6b
Spanos <nickspanosf1@gmail.com>
2025-07-04 20:24:15 +0300

    metadata in homepage

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -6,6 +6,12 @@ import {Hiring} from "@/components/Homepage/Hiring";
 import {InstagramPhotos} from "@/components/Homepage/InstagramPhotos";
 import {ContactForm} from "@/components/Homepage/ContactForm";
 import {Loading} from "@/components/Commons/Loading";
+import {getPageMetadata} from "@/lib/generateMetadata";
+
+export async function generateMetadata({params}) {
+    const { locale } = await params
+    return getPageMetadata(locale, 'Homepage');
+}
 
 export default function Home() {
     return (
