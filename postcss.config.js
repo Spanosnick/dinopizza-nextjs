b9cd1a12cb58a1ba6a098776b54885c69c836263
Spanos <nickspanosf1@gmail.com>
2025-07-04 20:42:18 +0300

    add dependency to remove unused css

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+// postcss.config.js
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        process.env.NODE_ENV === 'production' &&
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './pages/**/*.{js,ts,jsx,tsx}',
+                './components/**/*.{js,ts,jsx,tsx}',
+            ],
+            defaultExtractor: (content) =>
+                content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+    ],
+};
