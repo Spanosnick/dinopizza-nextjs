795307857003d6c3ab71b6e6c270c97d12f1df0a
Spanos <nickspanosf1@gmail.com>
2025-07-16 19:55:07 +0300

    add metadata to menu page

--- components/About/AllMenu.js
+++ components/About/AllMenu.js
@@ -1,11 +1,13 @@
 import productsData from '@/data/products.json';
 import Category from "@/components/Products/Category";
+import {Suspense} from "react";
+import {Loading} from "@/components/Commons/Loading";
 
 export function AllMenu() {
     return (<div className="section section-padding menu-v2">
             <div className="container">
                 {productsData.categories.map(category => {
-                        return <Category category={category} key={category.id} />
+                    return   <Suspense key={category.id} fallback={<Loading/>}> <Category category={category}  /> </Suspense>
                 })
                 }
             </div>
--- src/app/[locale]/menu/page.js
+++ src/app/[locale]/menu/page.js
@@ -2,13 +2,13 @@
 import {AllMenu} from "@/components/About/AllMenu";
 import {Suspense} from "react";
 import {Loading} from "@/components/Commons/Loading";
+import {getPageMetadata} from "@/lib/generateMetadata";
 
-
+export async function generateMetadata({params}) {
+    const { locale } = await params
+    return getPageMetadata(locale, 'MenuPage');
+}
 
 export default function Menu() {
-    return (
-        <Suspense fallback={<Loading/>}>
-            <AllMenu/>
-        </Suspense>
-    )
+    return <AllMenu/>
 }
