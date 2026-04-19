d66de2b87e3adc968c98b92ee509550c684a520e
Spanos <nickspanosf1@gmail.com>
2026-04-20 01:07:36 +0300

    Update routing configuration and add CLAUDE.md for project guidance

--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,9 +1,7 @@
 import {defineRouting} from 'next-intl/routing';
 
 export const routing = defineRouting({
-    // A list of all locales that are supported
     locales: ['en', 'el'],
-
-    // Used when no locale matches
-    defaultLocale: 'el'
+    defaultLocale: 'el',
+    localeDetection: false
 });
